126b3acb6a1ebc2e98b38755bc55de107ef51a6f
Your Name <you@example.com>
2022-11-29 17:39:24 +0600

    add

--- src/contexts/ProductContextProvider.js
+++ src/contexts/ProductContextProvider.js
@@ -1,22 +1,57 @@
 import axios from "axios";
-import React, { createContext, useContext } from "react";
-import { JSON_API_PRODUCTS } from "../helpers/consts";
+import React, { createContext, useContext, useReducer } from "react";
+import { ACTIONS, JSON_API_PRODUCTS } from "../helpers/consts";
+import { useLocation } from "react-router-dom";
 export const productContext = createContext();
 
 export const useProducts = () => {
   return useContext(productContext);
 };
 
+const INIT_STATE = {
+  products: [],
+  productDetails: {},
+};
+
+const reducer = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case ACTIONS.GET_PRODUCTS:
+      return { ...state, products: action.payload };
+
+    case ACTIONS.GET_PRODUCT_DETAILS:
+      return { ...state, productDetails: action.payload };
+
+    default:
+      return state;
+  }
+};
+
 //!adding products
 const ProductContextProvider = ({ children }) => {
+  const location = useLocation();
+  const [state, dispatch] = useReducer(reducer, INIT_STATE);
+
   async function addProduct(newProduct) {
     await axios.post(JSON_API_PRODUCTS, newProduct);
+    getProducts();
+    console.log(newProduct);
+  }
+  async function getProducts() {
+    const { data } = await axios(JSON_API_PRODUCTS);
+    dispatch({
+      type: ACTIONS.GET_PRODUCTS,
+      payload: data,
+    });
+
   }
   const values = {
     addProduct,
+    getProducts,
   };
 
-  return <productContext.Provider value={values}>{children}</productContext.Provider>;
+  return (
+    <productContext.Provider value={values}>{children}</productContext.Provider>
+  );
 };
 
 export default ProductContextProvider;
