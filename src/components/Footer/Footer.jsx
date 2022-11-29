78dff5c5326448012eac07428b576ee278a2c4ac
unknown <kanatbekuuludastan495@gmail.com>
2022-11-29 17:09:40 +0600

    add

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,82 +5,93 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import PinterestIcon from "@mui/icons-material/Pinterest";
 import YouTubeIcon from "@mui/icons-material/YouTube";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="the_left_footer">
+    <Box className="footer">
+      <Box className="the_left_footer">
         <h4>FARFETCH APP</h4>
-        <p>Farfetch App for iOS and Android</p>
-      </div>
-      <div className="the_center_footer">
+        <Typography>Farfetch App for iOS and Android</Typography>
+      </Box>
+      <Box className="the_center_footer">
         <h4>DESTINATION/REGION, CURRENCY AND LANGUAGE</h4>
         {/* <img src="" alt="" /> */}
-        <p>Kyrgyzstan, USD $</p>
+        <Typography>Kyrgyzstan, USD $</Typography>
         <h4>FOLLOW US</h4>
-        <div className="logo_center">
+        <Box className="logo_center">
           <>
-            <a href="https://www.instagram.com/farfetch/">
+            <Link href="https://www.instagram.com/farfetch/">
               <InstagramIcon sx={{ fill: "white" }} />
-            </a>
+            </Link>
           </>
           <>
-            <a href="https://www.instagram.com/farfetch/">
+            <Link href="https://www.instagram.com/farfetch/">
               <FacebookIcon sx={{ fill: "white" }} />
-            </a>
+            </Link>
           </>
           <>
-            <a href="https://www.instagram.com/farfetch/">
+            <Link href="https://www.instagram.com/farfetch/">
               <TwitterIcon sx={{ fill: "white" }} />
-            </a>
+            </Link>
           </>
 
           <>
-            <a href="https://www.instagram.com/farfetch/">
+            <Link href="https://www.instagram.com/farfetch/">
               <PinterestIcon sx={{ fill: "white" }} />
-            </a>
+            </Link>
           </>
 
           <>
-            <a href="https://www.instagram.com/farfetch/">
+            <Link href="https://www.instagram.com/farfetch/">
               <YouTubeIcon sx={{ fill: "white" }} />
-            </a>
+            </Link>
           </>
-        </div>
-      </div>
-      <div className="the_right_footer_first">
+        </Box>
+      </Box>
+      <Box className="the_right_footer_first">
         <h4>CUSTOMER SERVICE</h4>
-        <div className="first_p">
-          <p> Contact us </p> <p>Orders & delivery</p>{" "}
-          <p>
+        <Box className="first_Typography">
+          <Typography> Contact us </Typography>{" "}
+          <Typography>Orders & delivery</Typography>{" "}
+          <Typography>
             Holiday delivery dates & <br /> returns
-          </p>{" "}
-          <p> Payment & pricing</p> <p> Cryptocurrency payments</p>
-          <p>Returns & refunds</p> <p>FAQs</p> <p> Terms & conditions </p>
-          <p>
+          </Typography>{" "}
+          <Typography> Payment & pricing</Typography>{" "}
+          <Typography> Cryptocurrency payments</Typography>
+          <Typography>Returns & refunds</Typography>{" "}
+          <Typography>FAQs</Typography>{" "}
+          <Typography> Terms & conditions </Typography>
+          <Typography>
             Promotion terms & <br /> conditions
-          </p>{" "}
-          <p>Privacy policy Accessibility</p>
-        </div>
-      </div>
-      <div className="the_right_footer_second">
+          </Typography>{" "}
+          <Typography>Privacy policy Accessibility</Typography>
+        </Box>
+      </Box>
+      <Box className="the_right_footer_second">
         <h4>ABOUT FARFETCH</h4>
-        <div className="second_p">
-          <p> About us</p> <p>Investors</p> <p> Loyalty programme </p>{" "}
-          <p>Friend Referral Discount </p>{" "}
-          <p>
+        <Box className="second_p">
+          <Typography> About us</Typography> <Typography>Investors</Typography>{" "}
+          <Typography> Loyalty programme </Typography>{" "}
+          <Typography>Friend Referral Discount </Typography>{" "}
+          <Typography>
             FARFETCH Boutique <br /> partners
-          </p>{" "}
-          <p> Affiliate programme </p>
-          <p>Careers </p> <p>FARFETCH Customer Promise</p>
-          <p>FARFETCH app </p> <p>Sitemap</p> <p>Positively FARFETCH</p>
-          <p>
+          </Typography>{" "}
+          <Typography> Affiliate programme </Typography>
+          <Typography>Careers </Typography>{" "}
+          <Typography>FARFETCH Customer Promise</Typography>
+          <Typography>FARFETCH app </Typography>{" "}
+          <Typography>Sitemap</Typography>{" "}
+          <Typography>Positively FARFETCH</Typography>
+          <Typography>
             FARFETCH Platform <br />
             Solutions{" "}
-          </p>
-        </div>
-      </div>
-    </div>
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
