4e3238c74cba6de58af339030c023c8df5100775
Your Name <you@example.com>
2022-12-02 09:43:50 +0600

    cart

--- src/components/Cart/Order.jsx
+++ src/components/Cart/Order.jsx
@@ -1,12 +1,45 @@
-import { Box } from '@mui/material';
-import React from 'react';
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+import { useCart } from "../../contexts/CartContext";
 
 const Order = () => {
-    return (
-        <Box sx={{width:'35%'}}>
-            order
-        </Box>
-    );
+  const { cart } = useCart();
+  const promotion = 124.4;
+  return (
+    <Box sx={{ width: "30%", boxSizing: "border-box", padding: 6 }}>
+      <Typography sx={{ fontWeight: 600, fontSize: 18 }}>Summary</Typography>
+      <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+        <Typography>Subtotal</Typography>
+        <Typography>${cart.totalPrice}</Typography>
+      </Box>
+      <Box sx={{ mt: 1, display: "flex", justifyContent: "space-between" }}>
+        <Typography>Delivery</Typography>
+        <Typography>$0</Typography>
+      </Box>
+      <Box
+        sx={{
+          mt: 1,
+          display: "flex",
+          justifyContent: "space-between",
+          pb: 2,
+          borderBottom: "1px solid lightgrey",
+        }}
+      >
+        <Typography>Promotions</Typography>
+        <Typography>-${promotion}</Typography>
+      </Box>
+      <Box sx={{ mt: 2, display: "flex", justifyContent: "space-between" }}>
+        <Typography>Total</Typography>
+
+        <Typography sx={{ fontWeight: 600 }}>
+          {" "}
+          ${cart.totalPrice - promotion}
+        </Typography>
+      </Box>
+
+      <Button sx={{width:'100%', backgroundColor:'black', color:'white', p:1, mt:2}}>Go to Checkout</Button>
+    </Box>
+  );
 };
 
 export default Order;
